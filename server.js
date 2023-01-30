edcf0af46264cca4a19e0d3d26e6eed3a2384d2f
Ailye <ph.meillot@gmail.com>
2023-01-30 12:42:45 +0100

    Made from PH

--- server.js
+++ server.js
@@ -1,63 +1,45 @@
-const express = require('express')
-const { FieldValue } = require('firebase-admin/firestore')
-const app = express()
-const port = 8383
-const { db } = require('./firebase.js')
+const express = require("express");
+const { FieldValue } = require("firebase-admin/firestore");
+const app = express();
+const port = 8383;
+const { db } = require("./firebase.js");
+const cors = require("cors");
 
-app.use(express.json())
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
-const friends = {
-    'james': 'friend',
-    'larry': 'friend',
-    'lucy': 'friend',
-    'banana': 'enemy',
-}
+app.get("/wines", async (req, res) => {
+  const winesRef = db.collection("wines");
+  const snapshot = await winesRef.get();
+  let formattedData = [];
+  snapshot.forEach((doc) => {
+    formattedData.push(doc.data());
+  });
 
-app.get('/friends', async (req, res) => {
-    const peopleRef = db.collection('people').doc('associates')
-    const doc = await peopleRef.get()
-    if (!doc.exists) {
-        return res.sendStatus(400)
-    }
+  res.status(200).send(formattedData);
+});
 
-    res.status(200).send(doc.data())
-})
+app.get("/wines/:name", async (req, res) => {
+  const name = req.params["name"];
+  const wineRef = db.collection("wines");
+  const snapshot = await wineRef.where("name", "==", name).get();
+  let formattedData = [];
+  snapshot.forEach((doc) => {
+    formattedData.push(doc.data());
+  });
+  res.status(200).send(formattedData);
+});
 
-app.get('/friends/:name', (req, res) => {
-    const { name } = req.params
-    if (!name || !(name in friends)) {
-        return res.sendStatus(404)
-    }
-    res.status(200).send({ [name]: friends[name] })
-})
+app.post("/add-wine", async (req, res) => {
+  console.log(req.body);
+  const wine = req.body;
+  const wineRef = db.collection("wines");
+  wineRef.add(wine);
+  res.status(200).send(wine);
+});
 
-app.post('/addfriend', async (req, res) => {
-    const { name, status } = req.body
-    const peopleRef = db.collection('people').doc('associates')
-    const res2 = await peopleRef.set({
-        [name]: status
-    }, { merge: true })
-    // friends[name] = status
-    res.status(200).send(friends)
-})
-
-app.patch('/changestatus', async (req, res) => {
-    const { name, newStatus } = req.body
-    const peopleRef = db.collection('people').doc('associates')
-    const res2 = await peopleRef.set({
-        [name]: newStatus
-    }, { merge: true })
-    // friends[name] = newStatus
-    res.status(200).send(friends)
-})
-
-app.delete('/friends', async (req, res) => {
-    const { name } = req.body
-    const peopleRef = db.collection('people').doc('associates')
-    const res2 = await peopleRef.update({
-        [name]: FieldValue.delete()
-    })
-    res.status(200).send(friends)
-})
-
-app.listen(port, () => console.log(`Server has started on port: ${port}`))
+app.listen(port, () => console.log(`Server has started on port: ${port}`));
